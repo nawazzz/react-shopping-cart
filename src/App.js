e3b8a2456f3a93ae77a40519e536833483841811
nawazzz <65383832+nawazzz@users.noreply.github.com>
2021-03-10 12:10:14 +0530

    add size chart

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import List from "./List"
 function App() {
   return (
     <div className="App">
-      
-
-      <List/>
+      <div className="heroContainer">
+        <div className="sizeContainer">
+          <p className="sizes">Sizes:</p>
+          <div className="typesOfSize" >
+            <p className="sizeMeasurement"><br/>S</p>
+            <p className="sizeMeasurement"><br/>XS</p>
+            <p className="sizeMeasurement"><br/>M</p>
+            <p className="sizeMeasurement"><br/>L</p>
+            <p className="sizeMeasurement"><br/>XL</p>
+            <p className="sizeMeasurement"><br/>XXL</p>
+            <p className="sizeMeasurement"><br/>ML</p>
+          </div>
+        </div>
+        <List/>
+      </div>
     </div>
   );
 }
